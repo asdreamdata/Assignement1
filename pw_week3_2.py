8f2aeecad3d097d0c9e1d5cf1f1d78c08dcb4ade
asdreamdata <133505047+asdreamdata@users.noreply.github.com>
2023-06-23 22:53:07 +0530

    Update and rename pw_wwek3_2.py to pw_week3_2.py

--- pw_week3_2.py
+++ pw_week3_2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[10]:
-
-
 #Q1. Create a python program to sort the given list of tuples based on integer value using a lambda function.
 p_runs=[('Sachin Tendulkar', 34357), ('Ricky Ponting', 27483), ('Jack Kallis', 25534), ('Virat Kohli', 24936)]
 p_runs1=sorted(p_runs,key=lambda index:index[1])
